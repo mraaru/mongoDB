f4be104d3edd620a83d5732c2fb0e3f9c845936b
mraaru <anuragpandey123buy@gmail.com>
2020-11-06 03:01:47 +0530

    mm

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "anurag" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
