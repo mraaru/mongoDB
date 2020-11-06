89d550c0db756cb4f9116afd8c464b83661745a9
mraaru <anuragpandey123buy@gmail.com>
2020-11-06 17:33:15 +0530

    kk

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./services/passport");
+
 const app = express();
-
-passport.use(new GoogleStrategy());
-
+require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
